9e8b7212c4ecf8fa984d4dbd61a334897de3440f
chudi <chudiibida3@gmail.com>
2022-06-25 15:23:35 -0400

    done basics

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-// alienShip class and create 6 alien instances
-// should have hull, firepower, and accuracy props
-
-// object of user spaceship
-// should have hull, firepower, and accuracy props
-
 let activeEnemies = [] // array of ships still active
+let hidingEnemies = []
 
 let spaceship = {
     hull: 20,
@@ -14,16 +9,14 @@ let spaceship = {
         if (Math.random() < this.accuracy) { // if hit is good
             obj.hull -= this.firepower;
             if (obj.hull <= 0) { // if enemy is eliminated
-                console.log(`***Attack Successful*** Their hull has been destroyed`);
-                activeEnemies.shift();
+                newLog(`Clean Hit! <br> What do you want to do next?`, 'enemyElim')
+                obj.active = false;
             } else { // enemy hit but still active
-                console.log(`***Attack Successful*** Their hull has decreased to ${obj.hull}`)
-                console.log(`They will return fire...`)
+                newLog(`Hit! But they've returned fire... <br>`, 'enemyHit')
                 obj.attack()
             }
         } else { // missed hit
-            console.log('Your ship missed!')
-            console.log(`They will return fire...`)
+            newLog(`You missed & they've returned fire... <br>`, 'shipMiss')
             obj.attack()
         }
     }
@@ -33,25 +26,53 @@ class alienShip {
     constructor() {
         this.hull = Math.floor((Math.random() * 4) + 3); // HP btwn 3-6
         this.firepower = Math.floor((Math.random() * 3) + 2); // Attack btwn 2-4 
-        this.accuracy = Math.floor((Math.random() * 2) + 6) / 10; // Accuracy btwn 60-80%
+        this.accuracy = Math.floor((Math.random() * 2) + 6) / 10; // Have either 60, 70 or 80% Accuracy
+        this.active = true;
         activeEnemies.push(this)
     }
     attack() {
         if (Math.random() < this.accuracy) { // if hit is good
-            console.log('---You have been hit!');
             spaceship.hull -= this.firepower;
             if (spaceship.hull <= 0 ) {
-                console.log(`Game Over. Your ship was destroyed`)
+                newLog(`Your ship has been destroyed`, 'gameOver')
             } else {
-                console.log(`Your hull has decreased to ${spaceship.hull}`)
+                battleLog.innerHTML += `You've been hit, new HP is ${spaceship.hull}`
+                newLog('', 'shipHit')
             }
         } else { // missed hit
-            console.log('Alien missed!')
+            battleLog.innerHTML += 'The alien missed!'
+            newLog('', 'enemyMiss')
         }
     }
 }
 
-/***********************************************************/
+let battleLog = document.getElementsByClassName('battleLog')[0]
+battleLog.innerHTML = `Press 'Start' to Play`
+
+const newLog = (string, status) => {
+    if (string.length > 0) {
+        battleLog.innerHTML = string
+    }
+    if (status === 'enemyHit') {
+        console.log(`%c Attack Successful! Their hull has lowered`, 'color: green;')
+        console.log(`They will return fire...`)
+    } else if (status === 'enemyMiss') {
+        console.log('%c The alien missed!', 'color: orange;')
+    } else if (status === 'shipHit') {
+        console.log(`%c Your hull has decreased to ${spaceship.hull}`, 'color: red;' )
+    } else if (status === 'shipMiss') {
+        console.log('%c Your ship missed!', 'color: orange; font-style: italics;')
+        console.log(`They will return fire...`)
+    } else if (status === 'enemyElim') {
+        console.log(`%c Attack Successful! Their hull has been destroyed`, 'color: green;');
+    } else if (status === 'gameWon') {
+        console.log('%c Winner! Winner! Chicken Dinner!', 'color: gold;')
+    } else if (status === 'gameOver') {
+        console.log(`%c Game Over. Your ship was destroyed`, 'border: 1px solid grey;')
+    } else if (status === 'retreat') {
+        console.log('%c Game Over. Mission was abandoned', 'border: 1px solid grey;')
+    }
+}
 
 const checkVitals = () => {
     if (spaceship.hull > 0) {
@@ -59,41 +80,97 @@ const checkVitals = () => {
     } else {
      return false
     }
- }
+}
 
-/*
- */
 const startGame = () => {
-    startButton.style.display = "none"
-    const playerStats = document.querySelector('.playerStats')
-    playerStats.innerHTML = playerStats.innerHTML = 'Hull : ' + spaceship.hull + ' <br> ' + 'FirePower : ' + spaceship.firepower + ' <br> ' + 'Accuracy : ' + spaceship.accuracy
-    const enemyStats = document.querySelector('.enemyStats')
-    
+    /***********************************************************\
+     * CONNECTING TO DOM
+    ************************************************************/
 
+    startButton.style.display = "none"
+
+    const playerStats = document.querySelector('.playerStats')
+    const enemyStats = document.querySelector('.enemyStats')
     const enemyBoxes = document.getElementsByClassName('otherEnemies')[0]
+    let counter = document.getElementsByClassName('counter')[0]
+
+    battleLog.innerHTML = "Press 'Attack' to destroy the aliens"
+    playerStats.innerHTML = playerStats.innerHTML = 'Hull : ' + spaceship.hull + ' <br> ' + 'FirePower : ' + spaceship.firepower + ' <br> ' + 'Accuracy : ' + spaceship.accuracy
+    counter.innerHTML = "6 Aliens Remaining"
+
+    /***********************************************************\
+     * FUNCTIONS
+    ************************************************************/
 
     const launchedAttack = () => {
-        let numEnemiesBeforeAttack = activeEnemies.length
+        let currentAttacker = activeEnemies[0]
         if (checkVitals()) {
-            if (activeEnemies[0]) {
-                spaceship.attack(activeEnemies[0])
-                if (!(activeEnemies[0])) {
-                    console.log('Winner! Winner! Chicken Dinner!')
-                    playerStats.innerHTML = 'Hull : ' + spaceship.hull + ' <br> ' + 'FirePower : ' + spaceship.firepower + ' <br> ' + 'Accuracy : ' + spaceship.accuracy
-                    enemyStats.innerHTML = 'Hull : 0 <br> FirePower : 0 <br> Accuracy : 0'
-                } else {
-                    playerStats.innerHTML = 'Hull : ' + spaceship.hull + ' <br> ' + 'FirePower : ' + spaceship.firepower + ' <br> ' + 'Accuracy : ' + spaceship.accuracy
-                    enemyStats.innerHTML = 'Hull : ' + activeEnemies[0].hull + ' <br> ' + 'FirePower : ' + activeEnemies[0].firepower + ' <br> ' + 'Accuracy : ' + activeEnemies[0].accuracy
-                }
-                if (numEnemiesBeforeAttack > activeEnemies.length && activeEnemies[0]) {
-                    let itemDiv = document.getElementsByClassName('otherEnemies')[0];
-                    itemDiv.removeChild(itemDiv.firstElementChild)
-                    console.log(itemDiv)
+            if (activeEnemies.length > 0) {
+                if (currentAttacker.active) {
+                    spaceship.attack(activeEnemies[0])
+                    if (!(currentAttacker.active)) {
+                        eliminate()
+                        activeEnemies.shift();
+                        counter.innerHTML = `${activeEnemies.length} Aliens Remaining`
+                        retreatButton.style.display = "block"
+                    } else {
+                        retreatButton.style.display = "none"
+                    }
+                    if (activeEnemies.length === 0) {
+                        newLog(`Good Win Soldier!`, 'gameWon')
+                        enemyBoxes.style.display = "none"
+                        retreatButton.style.display = "none"
+                        attackButton.style.display = "none"
+                        playerStats.innerHTML = 'Hull : ' + spaceship.hull + ' <br> ' + 'FirePower : ' + spaceship.firepower + ' <br> ' + 'Accuracy : ' + spaceship.accuracy
+                        enemyStats.innerHTML = 'Hull : 0 <br> FirePower : 0 <br> Accuracy : 0'
+                        counter.innerHTML = `Mission Completed`
+                    } else if (activeEnemies.length === 1) {
+                        enemyBoxes.style.display = "none"
+                    } else {
+                        playerStats.innerHTML = 'Hull : ' + spaceship.hull + ' <br> ' + 'FirePower : ' + spaceship.firepower + ' <br> ' + 'Accuracy : ' + spaceship.accuracy
+                        enemyStats.innerHTML = 'Hull : ' + activeEnemies[0].hull + ' <br> ' + 'FirePower : ' + activeEnemies[0].firepower + ' <br> ' + 'Accuracy : ' + activeEnemies[0].accuracy
+                    }
+                    if (!(checkVitals())) {
+                        attackButton.style.display = "none"
+                        playerStats.innerHTML = 'Hull : 0 <br> ' + 'FirePower : ' + spaceship.firepower + ' <br> ' + 'Accuracy : ' + spaceship.accuracy
+                        counter.innerHTML = `Mission Failed`
+                    }
                 }
             }
+            
         }
         
     }
+
+    const eliminate = () => {
+        let itemDiv = document.getElementsByClassName('otherEnemies')[0]
+        if (hidingEnemies.length >= 1) {
+            itemDiv.removeChild(itemDiv.firstElementChild)
+            hidingEnemies.shift()
+        }
+    }
+    
+    const addHidingEnemies = () => {
+        // remaining aliens who aren't fighting
+        for (let i = 0; i < hidingEnemies.length; i++) {
+            let itemDiv = document.createElement('div')
+            itemDiv.classList.add("enemyImage")
+            enemyBoxes.appendChild(itemDiv)
+        }
+    }
+
+    const retreatGame = () => {
+        newLog(`Spaceship has disappeared`, 'retreat')
+        counter.innerHTML = 'Mission Abandoned'
+        attackButton.style.display = "none"
+        retreatButton.style.display = "none"
+
+        playerStats.innerHTML = ''
+    }
+
+    /***********************************************************
+     * CREATING ENEMIES
+    ************************************************************/
 
     let alien1 = new alienShip()
     let alien2 = new alienShip()
@@ -102,42 +179,29 @@ const startGame = () => {
     let alien5 = new alienShip()
     let alien6 = new alienShip()
 
-    // remaining aliens who aren't fighting
-    const inactiveEnemies = activeEnemies.slice(1);
-    inactiveEnemies.forEach((inactiveEnemy) => {
-        let itemDiv = document.createElement('div')
-        itemDiv.classList.add("enemyImage")
-        enemyBoxes.appendChild(itemDiv)
-    })
+    hidingEnemies = activeEnemies.slice(1);
+    addHidingEnemies()
 
     enemyStats.innerHTML = 'Hull : ' + activeEnemies[0].hull + ' <br> ' + 'FirePower : ' + activeEnemies[0].firepower + ' <br> ' + 'Accuracy : ' + activeEnemies[0].accuracy
 
-    const attackButton = document.getElementById('attack')
+    
     attackButton.style.display = "block";
     attackButton.addEventListener('click', launchedAttack)
-}
+    retreatButton.addEventListener('click', retreatGame)
 
+}
 const resetGame = () => {
-    activeEnemies = []
-    spaceship.hull = 20;
-
-    const attackButton = document.getElementById('attack')
-    startButton.style.display = "block"
-    attackButton.style.display = "none"
-    const playerStats = document.querySelector('.playerStats')
-    playerStats.innerHTML = ''
-    const enemyStats = document.querySelector('.enemyStats')
-    enemyStats.innerHTML = ''
-
-    const enemyBoxes = document.getElementsByClassName('otherEnemies')[0]
-    while (enemyBoxes.hasChildNodes()) {
-        enemyBoxes.removeChild(enemyBoxes.firstChild);
-    }
+    document.location.reload(true);
 }
+
+const retreatButton = document.getElementById('retreat')
+const attackButton = document.getElementById('attack')
 
 const startButton = document.getElementById('start');
 startButton.addEventListener('click', startGame)
 
 const resetButton = document.getElementById('reset') 
 resetButton.addEventListener('click', resetGame)
+
+
 
